5c0afbc3dcb248104e4d9c842142be83132b6667
MatiYagi <63799588+automateuszj@users.noreply.github.com>
2026-01-16 13:44:53 +0100

    Add files via upload

---
+++ tworcy.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sty 16, 2026 at 01:43 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `daza_banych`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tworcy`
+--
+
+CREATE TABLE `tworcy` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(123) NOT NULL,
+  `imie` varchar(123) NOT NULL,
+  `nazwisko` varchar(123) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tworcy`
+--
+
+INSERT INTO `tworcy` (`id`, `nazwa`, `imie`, `nazwisko`) VALUES
+(1, '67warrior', 'joanna', 'stolczyk'),
+(3, 'pielkie_waly', 'szymon', 'macipala');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
