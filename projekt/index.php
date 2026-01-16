5f37a41aeff346f3218d1109e900d203cf872493
MatiYagi <63799588+automateuszj@users.noreply.github.com>
2026-01-16 13:43:08 +0100

    Add files via upload

---
+++ projekt/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>qtas</p>
+</body>
+
+<?php
+$conn = new mysqli("localhost", "root", "", "daza_banych");
+if ($conn->connect_errno) {
+    die("Błąd połączenia: " . $conn->connect_error);
+}
+
+$sql = "SELECT nazwa FROM tworcy";
+
+$result = $conn->query($sql);
+
+if (!$result) {
+    die("Błąd zapytania: " . $conn->error);
+    }
+
+$fields = $result->fetch_fields();
+
+while ($row = $result->fetch_assoc()) {
+    echo "<p>" . $row['nazwa'] . "</p>";
+}
+
+$conn->close();
+
+?>
+</html>
