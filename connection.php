6110a0c452c062d60849b8f9c1071faa7513af30
Mateusz Jagieła <mateusz.jagiela@hotmail.com>
2026-02-02 21:27:40 +0100

    creator_registration v2

--- connection.php
+++ connection.php
@@ -3,5 +3,4 @@ $conn = new mysqli("localhost", "root", "", "siteDB");
 if ($conn->connect_errno) {
     die("Błąd połączenia: " . $conn->connect_error);
 }
-echo "Połączenie działa!"; // test
 ?>
--- index.php
+++ index.php
@@ -8,15 +8,3 @@ if (isset($_SESSION['user_id'])) {
 }
 exit;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>ZALOGUJ SIE PASOŻYCIE!!!</h1>
-</body>
-</html>
