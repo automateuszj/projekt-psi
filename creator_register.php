3f8786af76026dce5776a356fbf5109c7c84c66d
Konrad <konrad07grz@gmail.com>
2026-02-24 13:43:28 +0100

    creator_register.php html fixed and improved, weclome_style.css updated

--- creator_register.php
+++ creator_register.php
@@ -69,23 +69,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Zostań Twórcą</title>
+    <link rel="stylesheet" href="welcome_style.css">
 </head>
 <body>
 
-    <form method="post">
+    <a href="welcome.php" class="back-link">powrót na stronę główną</a>
 
-        <label for="phone" >Phone:</label><br>
-        <input type="number" id="phone" name="phone" required><br>
-        <label for="email" >email:</label><br>
-        <input type="email" id="email" name="email" required>
-        <button type="submit">Dodaj</button>
+    <div class="container">
+        <div class="add-post-container">
+            <h2>Zostań Twórcą</h2>
+            
+            <form method="post" class="creator-form">
+                <div class="input-group">
+                    <label for="phone">Telefon:</label>
+                    <input type="number" id="phone" name="phone" required>
+                </div>
 
-    </form>
+                <div class="input-group">
+                    <label for="email">E-mail:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+
+                <button type="submit" class="btn-add">Dodaj</button>
+            </form>
+        </div>
+    </div>
 
 </body>
 </html>
